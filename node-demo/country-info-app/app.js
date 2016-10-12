623f632cea21b076445a5d83ef840b89317dc800
Bram Pijper <brampijper@gmail.com>
2016-10-12 17:41:52 +0200

    Fixed the assignment by followingen the rules as stated in the assignment description

--- node-demo/country-info-app/app.js
+++ node-demo/country-info-app/app.js
@@ -1,3 +1,18 @@
 var helper = require(__dirname + '/helper.js')
+var filepath = __dirname + '/json/countries.json' 
 
-helper(process.argv[2])
+var countryName = process.argv[2]
+
+helper(filepath, function (parsedFile) {
+	for(var i = 0; i < parsedFile.length; i++ ) {
+		if (parsedFile[i].name == countryName) {
+			console.log('Country: ' + countryName)
+			console.log('TopLevelDomain: ' + parsedFile[i].topLevelDomain)
+		}
+	}
+})
+
+
+
+
+
--- node-demo/country-info-app/helper.js
+++ node-demo/country-info-app/helper.js
@@ -1,19 +1,14 @@
 var fs = require('fs');
 
-var readCountries = function (typedCountry) {
-	fs.readFile (__dirname + '/json/countries.json', function(err, file) {
+
+var readCountries = function (filename, callback) {
+	fs.readFile (filename, 'utf8', function(err, file) {
 		if(err) {
 			console.log(err)
 			throw err
 		}
-
-		var parsedFile = JSON.parse(file);
-		for(var i = 0; i < parsedFile.length; i++ ) {
-			if (parsedFile[i].name == typedCountry) {
-				console.log('Country: ' + typedCountry)
-				console.log('TopLevelDomain: ' + parsedFile[i].topLevelDomain)
-			}
-		}
+		var parsedFile = JSON.parse(file)
+		callback(parsedFile) 
 	})
 }
 module.exports = readCountries

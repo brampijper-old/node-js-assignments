82a98502d9053fe573b1e4bdfb8758d43bb7be38
Bram Pijper <brampijper@gmail.com>
2016-10-14 10:57:42 +0200

    Loaded customer json file

---
+++ node-demo/awesome-compound-interest/app.js
@@ -0,0 +1,13 @@
+// importing necessary modules
+const fs = require ('fs')
+
+//Read the customer data json
+fs.readFile(__dirname + '/customer.json', 'UTF8', (err, data) => {
+
+} ) 
+
+
+// Old format function. 
+// fs.readFile(__dirname + '/customer.json', 'UTF8', fucntion (err, data) {
+	
+// } ) 

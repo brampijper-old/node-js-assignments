6bc877a2b2b0296cf578be42b2b9534c6ec6a953
Bram Pijper <brampijper@gmail.com>
2016-10-17 12:40:39 +0200

    added error code

--- node-demo/express-demo/app.js
+++ node-demo/express-demo/app.js
@@ -2,9 +2,10 @@ const express = require ('express')
 const app = express()
 
 app.get ('/', (request, respons) => {
+	respons.status(418)
 	respons.send ('Hello Bram, it is so good to see you in the browser')
 } )
 
-app.listen(2000, () => {
+app.listen(8000, () => {
 	console.log('I am listening')
 })

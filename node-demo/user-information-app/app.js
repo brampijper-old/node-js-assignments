e1abc328130be3b3a3ff4911ef99211e2ca43edb
Bram Pijper <brampijper@gmail.com>
2016-10-18 18:00:25 +0200

    added code that compares the input from the search bar with the JSON bar, with the data in the JSON file

--- node-demo/user-information-app/app.js
+++ node-demo/user-information-app/app.js
@@ -1,11 +1,13 @@
 const express = require ('express')
 const fs = require ('fs')
 const app = express()
+const bodyParser = require ('body-parser')
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
+app.use(bodyParser())
 
-app.get('/index', (req, res) => {
+app.get('/users', (req, res) => {
 	fs.readFile(__dirname + '/users.json', (error, data) => {
 		if(error) throw error
 			let parsedData = JSON.parse(data)
@@ -13,7 +15,31 @@ app.get('/index', (req, res) => {
 	})
 })
 
+app.get('/form', (req, res) => {
+	res.render('form')
+})
+
+app.post ('/', (req, res) => {
+	let inputName = req.body.firstName
+	
+	fs.readFile(__dirname + '/users.json', (error, data) => {
+		if(error) throw error
+			let parsedData = JSON.parse(data)
+			for(var key in parsedData) {
+				if(parsedData[key].firstName || parsedData[key].lastName == inputName) {
+					res.render('result', {result: parsedData[key].firstName + parsedData[key].lastname + parsedData[key].emailadres})
+					console.log('Yaaaay we got a match')
+				}
+			}
+	})
+})
+
 app.listen(8000, () => {
 	console.log('Server running')
 })
+
+//takes in the post request from your form
+//displays matching users on a new page
+
+//I have to compare the firstname variable with the content of the JSON file.   
 

cc4c04c25b1e4b07041e4737c7f6c3e2eba290c5
Bram Pijper <brampijper@gmail.com>
2016-10-19 14:33:54 +0200

    Finished all the steps + added a description on every html page

--- node-demo/user-information-app/app.js
+++ node-demo/user-information-app/app.js
@@ -19,27 +19,42 @@ app.get('/form', (req, res) => {
 	res.render('form')
 })
 
-app.post ('/', (req, res) => {
-	let inputName = req.body.firstName
-	
+app.post ('/add', (req, res, inputName) => {
+	inputName = req.body.firstName
 	fs.readFile(__dirname + '/users.json', (error, data) => {
 		if(error) throw error
 			let parsedData = JSON.parse(data)
 			for(var key in parsedData) {
-				if(parsedData[key].firstName || parsedData[key].lastName == inputName) {
-					res.render('result', {result: parsedData[key].firstName + parsedData[key].lastname + parsedData[key].emailadres})
-					console.log('Yaaaay we got a match')
-				}
+				if(parsedData[key].firstName == inputName || parsedData[key].lastName == inputName) {
+					res.render(__dirname + '/views/result.pug', {result: parsedData[key]})
+				} 
 			}
 	})
 })
+
+app.get('/addUser', (req, res) => {
+	res.render('addUser')
+})
+
+app.post('/addUser', (req,res) => {
+
+	fs.readFile(__dirname + '/users.json', (error, data) => {
+		if(error) throw error
+			parsedData = JSON.parse(data)
+			parsedData.push({firstName: req.body.firstName, lastName: req.body.lastName, emailadres: req.body.emailadres})
+			json = JSON.stringify(parsedData)
+
+			fs.writeFile(__dirname + '/users.json', json, 'utf8', (error, data) => {
+				if(error) throw error
+					let test = JSON.parse(json)
+					res.render(__dirname + '/views/index.pug', {data: test})
+			})
+	})
+})
+
 
 app.listen(8000, () => {
 	console.log('Server running')
 })
 
-//takes in the post request from your form
-//displays matching users on a new page
-
-//I have to compare the firstname variable with the content of the JSON file.   
 

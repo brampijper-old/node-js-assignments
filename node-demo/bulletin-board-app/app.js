0a99766967f89022a9438c44346c0e118e905947
brampijper <brampijper@gmail.com>
2016-11-02 17:52:16 +0100

    Added some styling to the webpage

--- node-demo/bulletin-board-app/app.js
+++ node-demo/bulletin-board-app/app.js
@@ -1,17 +1,24 @@
+//Include libraries
 const fs = require ('fs')
 const express = require ('express')
 const pg = require ('pg')
 const app = express()
 const bodyParser = require ('body-parser')
 
-app.use(express.static('static'))
+//Load static files
+app.use('/resources', express.static( __dirname + '/static'))
+
+//Set the engine to pug and the view folder to the one in this location
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
+
 app.use(bodyParser.urlencoded({extended: true})); 
 
 //Database stuff
 let connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
 
+
+//Create main page
 app.get('/index', (req, res) => {
 	res.render('index')
 })
@@ -35,13 +42,12 @@ app.get('/showMessages', (req, res) => {
 
 app.post('/add', (req, res) => {
 	console.log('data sended to the database')
-	let post = {title: req.body.title, message: req.body.message}
 
 	//Connect to Database
 	pg.connect(connectionString, (err, client, done) => {
 		console.log('succesfully connected to db')
 
-		client.query('INSERT INTO messages (title, body) values ($1, $2)', [post.title, post.message], (err) => {
+		client.query('INSERT INTO messages (title, body) values ($1, $2)', [req.body.title, req.body.message], (err) => {
 			if(err) {
 				throw err;
 			} 
@@ -52,6 +58,7 @@ app.post('/add', (req, res) => {
 	})
 })
 
+//Start webserver on port 8000
 app.listen(8000, () => {
 	console.log('Yaay, server running')
 })

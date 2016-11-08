bb398b5cb20826409d0702f250838adc44640108
brampijper <brampijper@gmail.com>
2016-11-08 17:50:43 +0100

    user can register for a new profile and can log-in

--- node-demo/blog-application/app.js
+++ node-demo/blog-application/app.js
@@ -1,8 +1,10 @@
 //require the libraries.
 const express 		= require ('express')
 const sequelize 	= require ('sequelize')
-const bodyParser 	= require('body-parser')
-const session		= require('express-session')
+const bodyParser 	= require ('body-parser')
+const session		= require ('express-session')
+const promise 		= require ('promise')
+const cookieParser 	= require ('cookie-parser')
 const app 			= express()
 
 //connecting string to the database.
@@ -13,6 +15,12 @@ let db = new sequelize ('blog_application', process.env.POSTGRES_USER, process.e
 
 //Load the files in the resource folder.
 app.use('/resources', express.static(__dirname + '/resources'))
+
+//Make the bodyparser accessible everywhere
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.use(cookieParser())
+app.use(session({secret: 'i dont know what this is'}))
 
 //set the pug engine and the view folder with the .pug files. 
 app.set('view engine', 'pug')
@@ -41,9 +49,54 @@ app.get('/ping', (req, res) => {
 	res.send('pong')
 })
 
-//Syncing with the database
-db.sync({force: true}).then(function () {
-	console.log("succesfully synced with the db")
+//Log-in page
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+//register page
+app.get('/register', (req, res) => {
+	res.render('register')
+})
+
+app.get('/profile', (req, res) => {
+	res.render('profile', {
+		userName: req.session.userName
+	})
+})
+
+//adds new user to the database
+app.post('/create', (req, res) => {
+	if (req.body.username && req.body.email && req.body.password !== 0) {
+		User.create({
+			username: req.body.username,
+			email: req.body.email, 
+			password: req.body.password
+		}).then(function () {
+			db.sync().then(function() {
+				console.log('User Added')
+				res.redirect('/')
+			})
+		})
+	} 
+})
+
+//Check if log-in credentials matches
+app.post('/login', (req, res) => {
+	User.findOne({
+		where: {
+			email: req.body.email
+		}
+	}).then(function (user) {
+		if(user !== null && req.body.password === user.password) {
+			req.session.userName = user.username
+			console.log('succesfully logged in')
+			res.redirect('/profile')
+		} else {
+			console.log('wrong log-in credentials')
+			res.redirect('/')
+		}
+	})
 })
 
 //Start the web-server on port 8000

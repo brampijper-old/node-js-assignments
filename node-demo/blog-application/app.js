90baf27e58700d8bc7996014c59e414020dc3b95
brampijper <brampijper@gmail.com>
2016-11-08 15:52:06 +0100

    Defined the database structure and synced with the db for the first time

--- node-demo/blog-application/app.js
+++ node-demo/blog-application/app.js
@@ -2,10 +2,11 @@
 const express 		= require ('express')
 const sequelize 	= require ('sequelize')
 const bodyParser 	= require('body-parser')
+const session		= require('express-session')
 const app 			= express()
 
 //connecting string to the database.
-let db = new sequelize ('users', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+let db = new sequelize ('blog_application', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
 	server: 'localhost',
 	dialect: 'postgres'
 })
@@ -17,9 +18,32 @@ app.use('/resources', express.static(__dirname + '/resources'))
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
+//Define the database structure
+let User = db.define('user', {
+	username: 	sequelize.STRING,
+	email: 		sequelize.STRING,
+	password: 	sequelize.STRING 
+})
+
+let Message = db.define('message', {
+	note: 		sequelize.STRING,
+	user_id: 	sequelize.INTEGER
+})
+
+let Comment = db.define('comment', {
+	opinion: 	sequelize.STRING,
+	user_id: 	sequelize.INTEGER,
+	message_id: sequelize.INTEGER
+})
+
 //test if the webserver is working
 app.get('/ping', (req, res) => {
 	res.send('pong')
+})
+
+//Syncing with the database
+db.sync({force: true}).then(function () {
+	console.log("succesfully synced with the db")
 })
 
 //Start the web-server on port 8000
